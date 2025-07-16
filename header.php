1070e5337d8054d17c55ac2bc96163bd47f337d2
Jatin-Tahelyani <tahelyanijatin@gmail.com>
2025-07-16 22:56:56 +0530

    jul 16 10:56 PM

---
+++ header.php
@@ -0,0 +1,22 @@
+<head>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="nav-container">
+        <div class="nav-line left"></div>
+        <div class="custom-navbar">
+            <div class="navbar-logo">JavaCulture</div>
+            <div class="navbar-links">
+                <a href="#" class="nav-link">Home</a>
+                <a href="#" class="nav-link">Our Cafe</a>
+                <a href="#" class="nav-link">About Us</a>
+                <a href="#" class="nav-link">Trending</a>
+            </div>
+            <img src="images/user_logo.png" alt="User" class="user-img">
+        </div>
+        <div class="nav-line right"></div>
+    </div>
+</body>
+<script src="bootstrap.bundle.min.js" defer></script>
